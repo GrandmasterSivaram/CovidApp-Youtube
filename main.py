39090a834cb917e027f28ee8ef31bc63e51665f4
GrandmasterSivaram <46606237+GrandmasterSivaram@users.noreply.github.com>
2020-06-18 09:46:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ from kivy.uix.floatlayout import FloatLayout
 import requests
 from kivy.properties import ObjectProperty,NumericProperty,StringProperty
 from kivymd.uix.snackbar import Snackbar
-import json
+
 
 class WorldCases(FloatLayout):
-    pass
+    confirmed = NumericProperty()
+    deaths = NumericProperty()
+    recovered = NumericProperty()
+    date = StringProperty()
+
+    def search_world(self):
+        api_url = 'https://covid2019-api.herokuapp.com/total'
+        result = requests.get(url=api_url).json()
+        self.confirmed = result['confirmed']
+        self.deaths = result['deaths']
+        self.recovered = result['recovered']
+        self.date = result['dt']
+
 
 
 class LiveCases(FloatLayout):
@@ -20,15 +32,16 @@ class LiveCases(FloatLayout):
 
     def search(self):
         try:
-            url = 'https://covid2019-api.herokuapp.com/v2/country/{}'
-            result = requests.get(url=url.format(self.search_input.text)).json()
+            api_url = 'https://covid2019-api.herokuapp.com/v2/country/{}'
+            result = requests.get(url=api_url.format(self.search_input.text)).json()
             self.location = result['data']['location']
             self.confirmed = result['data']['confirmed']
             self.deaths = result['data']['deaths']
             self.recovered = result['data']['recovered']
         except:
-            Snackbar(text="No such country. Try again",font_size=20).show()
-            
+            Snackbar(text='No such country! Try again',font_size=20).show()
+
+
 
 
 
@@ -36,10 +49,8 @@ class MainApp(MDApp):
     def __init__(self,**kwargs):
         self.theme_cls = ThemeManager()
         self.theme_cls.primary_palette = 'Blue'
-        self.theme_cls.accent_palette = 'Green'
         self.theme_cls.theme_style = 'Dark'
         Window.size = (400,600)
         super().__init__(**kwargs)
-
 
 MainApp().run()
